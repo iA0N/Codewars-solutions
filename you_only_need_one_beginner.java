52096670a13a6186b9bafc35b59185f364f66ea5
ia0n <ia0n@protonmail.com>
2022-06-26 14:41:22 +0200

    Added Kata: https://www.codewars.com/kata/57cc975ed542d3148f00015b

---
+++ you_only_need_one_beginner.java
@@ -0,0 +1,18 @@
+/*
+Kata:
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+
+import java.util.*;
+public class Solution {
+
+    public static boolean check(Object[] a, Object x) {
+        return Arrays.asList(a).contains(x);
+    }
+
+}
