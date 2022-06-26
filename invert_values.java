9672549795fb879e9b3f37cd1a426fbaef2d6c75
ia0n <ia0n@protonmail.com>
2022-06-26 14:40:54 +0200

    Added Kata: https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad

---
+++ invert_values.java
@@ -0,0 +1,16 @@
+/*
+Kata:
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+*/
+
+
+import java.util.*;
+public class Kata {
+  public static int[] invert(int[] array) {
+    return Arrays.stream(array).map(w -> -w).toArray();
+  }
+}
+
